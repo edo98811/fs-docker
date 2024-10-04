84426a9ad5511b36750c39393cac487b37f6d9e9
edo98811 <edoardofilippi77@gmail.com>
2024-10-04 14:31:01 +0200

    changed container names

--- fs_docker_manager/docker.py
+++ fs_docker_manager/docker.py
@@ -17,7 +17,7 @@ class DockerInstance():
     
     def _command_function(self, max_number: int) -> list[str]:
         return [ 
-                "docker", "run", "--rm", "--name", f"edoardo_freesurfer_{max_number}",
+                "docker", "run", "--rm", "--name", f"docker_fs_container_{max_number}",
                 "-v", f"{self.SET["license_path"]}:/license.txt:ro",
                 "-v", f"{Path(__file__).parent}/freesurfer_all.sh:/root/freesurfer.sh",
                 "-v", f"{Path(__file__).parent.parent}/tmp:/info:ro",
@@ -44,7 +44,7 @@ class DockerInstance():
 
         # Set container index
         try:
-            container_list = subprocess.check_output("docker container ls --format '{{.Names}}' | grep '^edoardo_freesurfer_'", shell=True).decode().splitlines()
+            container_list = subprocess.check_output("docker container ls --format '{{.Names}}' | grep '^docker_fs_container_'", shell=True).decode().splitlines()
             max_number = 0
             for container in container_list:
                 number = int(container.split("_")[-1])
@@ -60,14 +60,14 @@ class DockerInstance():
             end = start + per_loop
             max_number += 1
             # print(f"Iteration: {i+1}")
-            print(f"running container edoardo_freesurfer_{max_number}")
+            print(f"running container docker_fs_container_{max_number}")
             # print(f"with parameters sh freesurfer.sh {function} {start} {end}")
 
             with open(log_file, "a") as log:
-                log.write(f"    edoardo_freesurfer_{max_number}\n")
+                log.write(f"    docker_fs_container_{max_number}\n")
             ## print(os.path.exists(f"{Path(__file__).parent.parent}/tmp"))
             command = [ # " ".join(
-                "docker", "run", "--rm", "--name", f"edoardo_freesurfer_{max_number}",
+                "docker", "run", "--rm", "--name", f"docker_fs_container_{max_number}",
                 "-v", f"{self.SET["license_path"]}:/license.txt:ro",
                 "-v", f"{Path(__file__).parent}/freesurfer_all.sh:/root/freesurfer.sh",
                 "-v", f"{Path(__file__).parent.parent}/tmp/:/info:ro",
@@ -80,7 +80,7 @@ class DockerInstance():
             if self.test_mode: command.append(True)
             # command = _command_function(max_number) + [function, str(start), str(end)]
 
-            with open(f"{logf}/edoardo_freesurfer_{max_number}.txt", "w") as f:
+            with open(f"{logf}/docker_fs_container_{max_number}.txt", "w") as f:
                 subprocess.Popen(command, stdout=f, stderr=f)
 
             start = end
@@ -100,7 +100,7 @@ class DockerInstance():
         if not os.path.exists(logf):
             os.makedirs(logf)
 
-        container_list = subprocess.check_output("docker container ls --format '{{.Names}}' | grep '^edoardo_freesurfer_'", shell=True).decode().splitlines()
+        container_list = subprocess.check_output("docker container ls --format '{{.Names}}' | grep '^docker_fs_container_'", shell=True).decode().splitlines()
 
         max_number = 0
         for container in container_list:
@@ -113,14 +113,14 @@ class DockerInstance():
 
         max_number += 1
 
-        print(f"running container edoardo_freesurfer_{max_number}")
+        print(f"running container docker_fs_container_{max_number}")
         # print(f"with parameters sh freesurfer.sh {function} {start} {end}")
 
         with open(log_file, "a") as log:
-            log.write(f"    edoardo_freesurfer_{max_number}\n")
+            log.write(f"    docker_fs_container_{max_number}\n")
 
         command = [ # " ".join(
-            "docker", "run", "--rm", "--name", f"edoardo_freesurfer_{max_number}",
+            "docker", "run", "--rm", "--name", f"docker_fs_container_{max_number}",
             "-v", f"{self.SET["license_path"]}:/license.txt:ro",
             "-v", f"{Path(__file__).parent}/freesurfer_all.sh:/root/freesurfer.sh",
             "-v", f"{Path(__file__).parent.parent}/tmp:/info:ro",
@@ -133,7 +133,7 @@ class DockerInstance():
         if self.test_mode: command.append(True)
         # command = _command_function(max_number) + [function]
 
-        # with open(f"{logf}/edoardo_freesurfer_{max_number}.txt", "w") as f:
+        # with open(f"{logf}/docker_fs_container_{max_number}.txt", "w") as f:
         subprocess.Popen(command)
 
         with open(log_file, "a") as log:
